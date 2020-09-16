690353d25295e0dcd59c736405ff93321c07a0d5
jamessai4 <48791720+jamessai4@users.noreply.github.com>
2020-09-16 23:54:43 +0700

    change server

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
-const express = require ('express')
-const serverStatic = require('serve-static')
-const path = require('path')
-const serveStatic = require('serve-static')
-const { prototype } = require('core-js/fn/promise')
-
-const app = express()
-
-// app.use('/',serveStatic(path.join(_dirname, '/dist')))
-
-const port = process.env.PORT || 8080
-app.listen(port)
-console.log('Listening on port: ' + prototype)
+const express = require('express');
+const serveStatic = require("serve-static")
+const path = require('path');
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 8080;
+app.listen(port);
